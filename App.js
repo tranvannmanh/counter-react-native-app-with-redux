0da4a8f47f42dcd673b3cab870981e4b7739b91f
tranvanmanh <manh.tranvann@gmail.com>
2022-02-22 11:54:23 +0700

    completed

--- App.js
+++ App.js
@@ -13,22 +13,25 @@ const Counter = () => {
   const counter = useSelector(state => state.counter);
   const [numberAction, setNumberAction] = useState('1');
   const dispatch = useDispatch();
+
   return (
       <View style={styles.center}>
-        <Text>{counter}</Text> 
-        <TextInput
+        <Text style={styles.Counter}>{counter}</Text> 
+        <TextInput style={styles.InputField}
           onChangeText={e=>setNumberAction(e)}
         />
-        <View>
+        <View style={styles.ButtonOpacity}>
           <TouchableOpacity
+            style={styles.Button}
             onPress={()=>dispatch(increment(Number(numberAction)))}
           >
-            <Text>Incre</Text>
+            <Text style={{color: '#FF0000'}}>Incre</Text>
           </TouchableOpacity>
           <TouchableOpacity
+            style={styles.Button}
             onPress={()=>dispatch(decrement(Number(numberAction)))}
           >
-            <Text>Decre</Text>
+            <Text style={{color: '#FF0000'}}>Decre</Text>
           </TouchableOpacity>
         </View>
       </View> 
@@ -48,6 +51,36 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+
+  ButtonOpacity: {
+    display: 'flex',
+    width: '50%',
+    flexDirection: 'row',
+  },
+
+  Counter: {
+    fontSize: 100,
+  },
+
+  InputField: {
+    fontSize: 20,
+    backgroundColor: '#B1B1B1',
+    width: '50%',
+    textAlign: 'center',
+    borderRadius: 10,
+    marginVertical: 10,
+    // height: 30,
+  },
+
+  Button: {
+    backgroundColor: '#3FC63A',
+    width: '50%',
+    display: 'flex',
+    alignItems: 'center',
+    borderRadius: 50,
+
+  }
+
 });
 
 export default App;
